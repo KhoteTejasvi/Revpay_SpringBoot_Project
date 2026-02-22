366622ab4a57185442cddfb6cccb73f258758857
KhoteTejasvi <tejasvikhote2616@gmail.com>
2026-02-23 01:30:32 +0530

    updated

---
+++ src/main/java/com/revpayproject/revpay/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.revpayproject.revpay.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex) {
+
+        ErrorResponse error = new ErrorResponse(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
