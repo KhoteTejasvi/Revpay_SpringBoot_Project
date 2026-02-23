a55fcc475c87f72b01202b0b55d537e4852ecbd6
KhoteTejasvi <tejasvikhote2616@gmail.com>
2026-02-23 18:26:37 +0530

    updated PaymentMethodController

---
+++ src/main/java/com/revpayproject/revpay/controller/PaymentMethodController.java
@@ -0,0 +1,39 @@
+package com.revpayproject.revpay.controller;
+
+import com.revpayproject.revpay.dto.AddCardDto;
+import com.revpayproject.revpay.entity.PaymentMethod;
+import com.revpayproject.revpay.service.PaymentMethodService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/payment")
+@RequiredArgsConstructor
+public class PaymentMethodController {
+
+    private final PaymentMethodService paymentMethodService;
+
+    private String getLoggedInEmail() {
+        return SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getName();
+    }
+
+    @PostMapping("/add")
+    public String addCard(@RequestBody AddCardDto dto) {
+        return paymentMethodService.addCard(getLoggedInEmail(), dto);
+    }
+
+    @GetMapping("/my-cards")
+    public List<PaymentMethod> getCards() {
+        return paymentMethodService.getUserCards(getLoggedInEmail());
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteCard(@PathVariable Long id) {
+        return paymentMethodService.deleteCard(id);
+    }
+}
