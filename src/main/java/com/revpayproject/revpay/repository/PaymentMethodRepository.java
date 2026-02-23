b6c176bb6b64d026f04fee893be2519d085add99
KhoteTejasvi <tejasvikhote2616@gmail.com>
2026-02-23 17:05:30 +0530

    Create PaymentMethodRepositor

---
+++ src/main/java/com/revpayproject/revpay/repository/PaymentMethodRepository.java
@@ -0,0 +1,12 @@
+package com.revpayproject.revpay.repository;
+
+import com.revpayproject.revpay.entity.PaymentMethod;
+import com.revpayproject.revpay.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
+
+    List<PaymentMethod> findByUser(User user);
+}
